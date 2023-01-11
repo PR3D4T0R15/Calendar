1eab18e9dea482708f7b0ed8025e923e1ff36dbc
Matt <32712980+PR3D4T0R15@users.noreply.github.com>
2023-01-11 17:19:11 +0100

    Zmiana wielkosci kontrolki dni podczas zmiany rozmiaru okna

--- CalendarEX/Form2.cs
+++ CalendarEX/Form2.cs
@@ -135,6 +135,9 @@ namespace CalendarEX
                 PustyDzien pustaKontrolkaDni = new PustyDzien();
                 PodzialTygodni_kontrolkiDni.Controls.Add(pustaKontrolkaDni);
                 pustaKontrolkaDni.Name = "pusta_" + i.ToString();
+
+                pustaKontrolkaDni.Width = (PodzialTygodni_kontrolkiDni.Width / 7) - 2;
+                pustaKontrolkaDni.Height = (PodzialTygodni_kontrolkiDni.Height / 6) - 2;
             }
 
             //wypelnienie dni danego miesiaca
@@ -144,8 +147,17 @@ namespace CalendarEX
                 PodzialTygodni_kontrolkiDni.Controls.Add(kontrolkaDni);
                 kontrolkaDni.UstawNumerDnia(i);
                 kontrolkaDni.Name = "dzien_" + i.ToString();
+
+                kontrolkaDni.Width = (PodzialTygodni_kontrolkiDni.Width / 7) - 2;
+                kontrolkaDni.Height = (PodzialTygodni_kontrolkiDni.Height / 6) - 2;
             }
 
+        }
+
+        private void OknoMiesiac_Resize(object sender, EventArgs e)
+        {
+            PodzialTygodni_kontrolkiDni.Controls.Clear();
+            WyswietlKalendarz();
         }
     }
 }
